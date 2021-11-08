6b70de2bfa3444bbee0ecbd2850d2dc974d705ad
Diana <dianapaodcs@gmail.com>
2021-11-08 07:04:20 -0600

    Cipher textFile

---
+++ src/main/java/AESFunction.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.Base64;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
+/**
+ *
+ * @author Diana Paola
+ */
+public class AESFunction {
+    private SecretKeySpec crearClave(String clave) throws UnsupportedEncodingException, NoSuchAlgorithmException {
+        byte[] claveEncriptacion = clave.getBytes("UTF-8");
+        
+        MessageDigest sha = MessageDigest.getInstance("SHA-1");
+        
+        claveEncriptacion = sha.digest(claveEncriptacion);
+        claveEncriptacion = Arrays.copyOf(claveEncriptacion, 16);
+        
+        SecretKeySpec secretKey = new SecretKeySpec(claveEncriptacion, "AES");
+
+        return secretKey;
+    }
+    
+    public String cifrar(String datos, String claveSecreta) throws UnsupportedEncodingException, NoSuchAlgorithmException, InvalidKeyException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException {
+        SecretKeySpec secretKey = this.crearClave(claveSecreta);
+        
+        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");        
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+
+        byte[] dataEncrypt = datos.getBytes("UTF-8");
+        byte[] bytesEncrypted = cipher.doFinal(dataEncrypt);
+        String masageEncryptedString = Base64.getEncoder().encodeToString(bytesEncrypted);
+
+        return masageEncryptedString;
+    }
+    
+    public String descifrar(String dataEncrypted, String originalSecretPass) throws UnsupportedEncodingException, NoSuchAlgorithmException, InvalidKeyException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException {
+        SecretKeySpec secretKey = this.crearClave(originalSecretPass);
+
+        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+        
+        byte[] bytesEncryped = Base64.getDecoder().decode(dataEncrypted.trim());
+        byte[] dataDecrypted = cipher.doFinal(bytesEncryped);
+        String dataInString = new String(dataDecrypted);
+        
+        return dataInString;
+    }
+    
+}
